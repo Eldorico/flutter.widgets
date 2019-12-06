15fcd20558bfbb333f3ea50b7819ed14a7105be9
eldorico <eldorico@gmail.com>
2019-12-06 13:44:59 +0100

    Revert "added customMaxExtent in ItemPositionListener. (I know it isnt a good place but I'll start with that)"
    This reverts commit ef1386244e11e0bf357faca55c9dbbadd190fb78.

--- lib/src/scrollable_positioned_list/src/item_positions_listener.dart
+++ lib/src/scrollable_positioned_list/src/item_positions_listener.dart
@@ -14,10 +14,6 @@ abstract class ItemPositionsListener {
   /// [ScrollablePositionedList] to return the current position of items.
   factory ItemPositionsListener.create() => ItemPositionsNotifier();
 
-  double get customMaxExtend;
-  double set customMaxExtend(double customMaxExtend);
-  void resetCustomMaxExtend();
-
   /// The position of items that are at least partially visible in the viewport.
   ValueListenable<Iterable<ItemPosition>> get itemPositions;
 }
